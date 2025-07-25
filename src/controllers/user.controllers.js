b4ebfb99143bab1b6dfa4a3c20f5a700c1b25d27
araiz45 <araizzahid45@gmail.com>
2025-07-25 06:03:33 -0700

    logic building part 2

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,6 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/users.models.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   // TODO: Get user details from users.
@@ -22,7 +24,6 @@ const registerUser = asyncHandler(async (req, res) => {
   // TODO: return Response.
 
   const { fullName, email, username, password } = req.body;
-  console.log("email: ", email);
 
   if (
     [fullName, email, username, password].some(
@@ -33,7 +34,40 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   const existedUser = User.findOne({ $or: [{ username }, { email }] });
-  // https://youtu.be/7fjOw8ApZ1I?si=wLf2XvDT2urxWWA-&t=32790
+  if (existedUser) {
+    throw new ApiError(409, "username or email already existed");
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) throw new ApiError(400, "Avatar file is required");
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!avatar) throw new ApiError(400, "Avatar file is required");
+
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser)
+    throw new ApiError(500, "while registering user, something went wrong");
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "user has been registered"));
+
   res.status(200).json({ message: "ok" });
 });
 
